2f151dd46c1d3e1bfa82bde05df1884920112543
Matt Mowers <Matthew.Mowers@nrel.gov>
2017-07-07 13:42:50 -0400

    Add content scenarios to run jedi with different local shares.

--- reeds_jedi.py
+++ reeds_jedi.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import gdxpds
+import numpy as np
 import pandas as pd
 import win32com.client as win32
 import os
@@ -45,6 +46,7 @@ df = df.reindex(columns=df.columns.values.tolist() + out_cols)
 
 #first, open jedi workbook
 excel = win32.Dispatch('Excel.Application')
+#excel.Visible = True
 wb = excel.Workbooks.Open(this_dir + r'/jedi_models/01D_JEDI_Land-based_Wind_Model_rel._W12.23.16.xlsm')
 ws_in = wb.Worksheets('ProjectData')
 ws_out = wb.Worksheets('SummaryResults')
@@ -57,20 +59,38 @@ ws_in.Range('B17').Value = 1 #number of projects
 ws_in.Range('B18').Value = 3000 #Turbine size (kW)
 ws_in.Range('B22').Value = 2015 #dollar year
 ws_in.Range('B24').Value = 'N' #Y to use default local shares etc, and N to not use them.
-           
-#now, fill in new capital and o&m cost, and get associated economic impact.
-for i, r in df.iterrows():
-    if (i+1)%100 == 0:
-        print(str(i+1) + '/'+str(len(df.index)))
-    if r['cat']=='Capital':
-        ws_in.Range('B20').Value = r['price']
-        out_row_start = 28
-    elif r['cat']=='OM':
-        ws_in.Range('B21').Value = r['price']
-        out_row_start = 34
-    #we need to scale the outputs by the capacity, since we used a 100 MW project to create the outputs
-    mult = r['capacity']/float(ws_in.Range('B16').Value)
-    for col in range(4):
-        for row in range(3):
-            df.iloc[i,7 + col*3 + row] = mult * float(ws_out.Cells(out_row_start + row, 2 + col).Value)
+
+#read in array of local shares. Scenarios start in the 5th column (index=4)
+df_local_share = pd.read_csv('local_share.csv')
+content_scenarios = df_local_share.columns.values.tolist()[4:]
+df = df.reindex(columns=['content_scenario'] + df.columns.values.tolist())
+df_full = pd.DataFrame()
+for scen_name in content_scenarios:
+    #set content_scenario column
+    df['content_scenario'] = scen_name
+    #fill in the local share cells
+    for i, r in df_local_share.iterrows():
+        ws_in.Range('E' + str(r['row'])).Value = r[scen_name]
+    #now, loop through df rows, fill in new capital and o&m cost, and get associated economic impacts
+    for i, r in df.iterrows():
+        if (i+1)%100 == 0:
+            print(str(i+1) + '/'+str(len(df.index)))
+        if r['cat']=='Capital':
+            ws_in.Range('B20').Value = r['price']
+            out_row_start = 28
+        elif r['cat']=='OM':
+            ws_in.Range('B21').Value = r['price']
+            out_row_start = 34
+        #we need to scale the outputs by the capacity, since we used a 100 MW project to create the outputs
+        mult = r['capacity']/float(ws_in.Range('B16').Value)
+        for col in range(4):
+            for row in range(3):
+                df.iloc[i,8 + col*3 + row] = mult * float(ws_out.Cells(out_row_start + row, 2 + col).Value)
+    df_full = pd.concat([df_full, df], ignore_index=True)
+    #clear jedi values from df
+    for c in out_cols:
+        df[c] = np.nan
 wb.Close(False)
+df = df_full
+df.to_csv('out.csv', index=False)
+
